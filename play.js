7087483ee1c2382aab8cddebfa7e101ac004faab
Victoria Lee <victorialee72@live.com>
2020-07-29 00:40:32 +0000

    Refactored code

--- play.js
+++ play.js
@@ -1,28 +1,3 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-
-
-
-  conn.on('data', (data) => {
-    console.log('Server says: ', data)
-  })
-
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  
-
-  return conn;
-}
-
-
-
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
