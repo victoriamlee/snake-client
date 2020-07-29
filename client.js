371c456b48d9e950e74d6accaae37d0ceb673633
Victoria Lee <victorialee72@live.com>
2020-07-29 00:55:10 +0000

    Added name

--- client.js
+++ client.js
@@ -10,6 +10,14 @@ const connect = function() {
     console.log('Server says: ', data);
   });
 
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+  });
+
+  conn.on('connect', () => {
+    conn.write("Name: VL");
+  });
+
   // interpret incoming data as text
   conn.setEncoding('utf8');
 
